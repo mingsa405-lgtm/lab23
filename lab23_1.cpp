f20f8d1d5dd72521c4aad296e7b496bc739eb256
ming <mingsa405@gmail.com>
2026-02-28 16:42:01 +0700

    ีup

--- lab23_1.cpp
+++ lab23_1.cpp
@@ -19,20 +19,113 @@ string toUpperStr(string x){
     for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
     return y;
 }
+void importDataFromFile(const string& filename,
+vector<string> &names,
+vector<int> &scores,
+vector<char> &grades){
+    ifstream file(filename);
+    string data;
+    
+    while(getline(file,data)){
+        char name[100];
+        const char* text = data.c_str();
+        char format[]="%[^:]: %d %d %d";
+        int sc1,sc2,sc3;
+        sscanf(text,format,name,&sc1,&sc2,&sc3);
+        int score=sc1+sc2+sc3;
+        char grade=score2grade(score);
+        names.push_back(name);
+        scores.push_back(score);
+        grades.push_back(grade);
+        
+        
+        
+        
+        
+        
+    }file.close();
 
-void importDataFromFile(){
+
+
+
+}
+void getCommand(string &command, string &key){
+    cout<< "Please input your command:\n";
+      string line;
+    getline(cin, line);
+
+    if (line.empty()) {
+        command = "";
+        key = "";
+        return;
+    }
+
+    
+    size_t pos = line.find(' ');
+    if (pos == string::npos) {
+        command = line;
+        key = "";
+    } else {
+        command = line.substr(0, pos);
+        key = line.substr(pos + 1);
+    }
+}
+
+
+
+
+
+void searchName(vector<string> &names,
+vector<int> &scores,
+vector<char> &grades,
+string key
+
+){  bool found=false;
+    cout<<"---------------------------------\n";
+
+    for(size_t i=0;i<names.size();i++){
+    if(key==toUpperStr(names[i])){
+        cout<<names[i]<<"'s score = "<<scores[i]<<"\n";
+        cout<<names[i]<<"'s grade = "<<grades[i];
+        found=true;
+    }
+    
+    
+}if(found==false){ cout<<"Cannot found.";}
+cout<<"\n---------------------------------\n";
+
+
+
+
 
 }
 
-void getCommand(){
 
+
+void searchGrade(
+    vector<string> &names,
+    vector<int> &scores, 
+    vector<char> &grades
+    , string key){
+    bool found=false;
+    cout<<"---------------------------------\n";
+ for(size_t i=0;i<names.size();i++){
+    if( grades[i] == toupper(key[0])){
+        cout<<names[i]<<" ("<<scores[i]<<")\n";
+        found=true;
+    }
+
+    
+    
+    
+    
+    
 }
+if(found==false){cout<<"Cannot found.\n" ;   }
+cout<<"---------------------------------\n";
+ 
 
-void searchName(){
 
-}
-
-void searchGrade(){
 
 }
 
@@ -46,7 +139,7 @@ int main(){
     
     do{
         string command, key;
-        getCommand(command,key);
+        getCommand( command, key);
         command = toUpperStr(command);
         key = toUpperStr(key);
         if(command == "EXIT") break;
